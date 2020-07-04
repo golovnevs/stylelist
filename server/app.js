e60dfa1d4ddd63c7dd0a4e33b55c7d9e7465627a
yankovskyandrey <YankovskyAndrey@gmail.com>
2020-07-04 21:06:02 +0300

    Add an example of api server

--- server/app.js
+++ server/app.js
@@ -2,7 +2,25 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.post('/save-results', (req, res) => res.send('abc'))
+const db = [
+  {
+    name: 'vasya',
+    result: 5
+  }
+]
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.all('*', (req, res, next) => {
+  res.set('Access-Control-Allow-Origin', '*')
+  next()
+})
+
+app.get('/get-results', (req, res) => {
+  res.send(db)
+})
+
+app.post('/save-results', (req, res) => {
+  db.push({ name: 'Petya', result: 4 })
+  res.sendStatus(201)
+})
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${ port }`))
